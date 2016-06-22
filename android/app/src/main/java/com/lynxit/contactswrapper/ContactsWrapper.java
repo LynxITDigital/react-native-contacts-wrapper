81057cb454707b640675363277e4e782e5f04102
Oliver Jacobs <oliver.jacobs1@icloud.com>
2016-06-22 11:30:48 +1000

    Added simple getContact function, which returns a JS object containing basic contact data.

--- android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
+++ android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
@@ -14,135 +14,177 @@ import com.facebook.react.*;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.ViewManager;
 
 public class ContactsWrapper extends ReactContextBaseJavaModule implements ActivityEventListener {
-
+    
     private static final int CONTACT_REQUEST = 1;
+    private static final int EMAIL_REQUEST = 2;
     private static final String E_CONTACT_CANCELLED = "E_CONTACT_CANCELLED";
     private static final String E_CONTACT_NO_DATA = "E_CONTACT_NO_DATA";
     private static final String E_CONTACT_NO_EMAIL = "E_CONTACT_NO_EMAIL";
     private static final String E_CONTACT_EXCEPTION = "E_CONTACT_EXCEPTION";
     private Promise mContactsPromise;
     private Activity mCtx;
-
-
+    
+    
     public ContactsWrapper(ReactApplicationContext reactContext) {
         super(reactContext);
         reactContext.addActivityEventListener(this);
     }
-
+    
     @Override
     public String getName() {
         return "ContactsWrapper";
     }
-
+    
+    
+    
+    @ReactMethod
+    public void getContact(Promise contactsPromise) {
+        launchPicker(contactsPromise, CONTACT_REQUEST);
+    }
+    
     @ReactMethod
     public void getEmail(Promise contactsPromise) {
+        launchPicker(contactsPromise, EMAIL_REQUEST);
+    }
+    
+    /**
+     * Lanch the contact picker, with the specified requestCode for returned data.
+     * @param contactsPromise - promise passed in from React Native.
+     * @param requestCode - request code to specify what contact data to return
+     */
+    private void launchPicker(Promise contactsPromise, int requestCode) {
         mContactsPromise = contactsPromise;
-
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
         mCtx = getCurrentActivity();
         if (intent.resolveActivity(mCtx.getPackageManager()) != null) {
-            mCtx.startActivityForResult(intent, CONTACT_REQUEST);
+            mCtx.startActivityForResult(intent, requestCode);
         }
-
-
     }
-
+    
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
-        if (requestCode != CONTACT_REQUEST) {
-            return;
-        }
-
+        
+        
         String email = null;
         switch (resultCode) {
             case (Activity.RESULT_OK):
-                try {
-                    Uri contactUri = intent.getData();
-
-
-                    // get the contact id from the Uri
-                    String id = contactUri.getLastPathSegment();
-
-                    // query for everything email
-                   Cursor cursor = mCtx.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                            null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[] { id },
-                            null);
-
-                    int emailIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
-
-                    // let's just get the first email
-                    if (cursor.moveToFirst()) {
-                        email = cursor.getString(emailIdx);
-                        mContactsPromise.resolve(email);
-                        return;
-                    } else {
-                        mContactsPromise.reject(E_CONTACT_NO_EMAIL, "No email found for contact");
-                        return;
-                    }
-
-
-                /*
-                    //First get ID
-                    String id = null;
-                    int idx;
-                    Cursor cursor = mCtx.getContentResolver().query(contactUri, null, null, null, null);
-                    if (cursor.moveToFirst()) {
-                        idx = cursor.getColumnIndex(ContactsContract.Contacts._ID);
-                        id = cursor.getString(idx);
-                    } else {
-                        mContactsPromise.reject(E_CONTACT_NO_DATA, "Contact Data Not Found");
-                        return;
-                    }
-
-                    // Build the Entity URI.
-                    Uri.Builder b = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, id).buildUpon();
-                    b.appendPath(ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
-                    contactUri = b.build();
-
-                    // Create the projection (SQL fields) and sort order.
-                    String[] projection = {
-                            ContactsContract.Contacts.Entity.RAW_CONTACT_ID,
-                            ContactsContract.Contacts.Entity.DATA1,
-                            ContactsContract.Contacts.Entity.MIMETYPE };
-                    String sortOrder = ContactsContract.Contacts.Entity.RAW_CONTACT_ID + " ASC";
-                    cursor = mCtx.getContentResolver().query(contactUri, projection, null, null, sortOrder);
-
-                    String mime;
-                    int mimeIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.MIMETYPE);
-                    int dataIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.DATA1);
-                    if (cursor.moveToFirst()) {
-                        do {
-                            mime = cursor.getString(mimeIdx);
-                            if (mime.equalsIgnoreCase(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)) {
-                                email = cursor.getString(dataIdx);
+                Uri contactUri = intent.getData();
+                switch(requestCode) {
+                    case(CONTACT_REQUEST):
+                        try {
+                            /* Retrieve all possible data about contact and return as a JS object */
+                            
+                            /* Map Any contact data we want returned to the JS object key for React Native */
+                            HashMap<String, String> returnKeys = new HashMap<String, String>();
+                            returnKeys.put(ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, "name");
+                            returnKeys.put(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, "phone");
+                            returnKeys.put(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, "email");
+                            
+                            //First get ID
+                            String id = null;
+                            int idx;
+                            WritableMap contactData = Arguments.createMap();
+                            Cursor cursor = mCtx.getContentResolver().query(contactUri, null, null, null, null);
+                            if (cursor.moveToFirst()) {
+                                idx = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                                id = cursor.getString(idx);
+                            } else {
+                                mContactsPromise.reject(E_CONTACT_NO_DATA, "Contact Data Not Found");
+                                return;
                             }
-
-                        } while (cursor.moveToNext());
-                    }
-
-                    if(email != null) {
-                        mContactsPromise.resolve(email);
+                            
+                            // Build the Entity URI.
+                            Uri.Builder b = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, id).buildUpon();
+                            b.appendPath(ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
+                            contactUri = b.build();
+                            
+                            // Create the projection (SQL fields) and sort order.
+                            String[] projection = {
+                                ContactsContract.Contacts.Entity.MIMETYPE,
+                                ContactsContract.Contacts.Entity.DATA1
+                            };
+                            String sortOrder = ContactsContract.Contacts.Entity.RAW_CONTACT_ID + " ASC";
+                            cursor = mCtx.getContentResolver().query(contactUri, projection, null, null, sortOrder);
+                            
+                            String mime;
+                            boolean foundData = false;
+                            int dataIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.DATA1);
+                            int mimeIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.MIMETYPE);
+                            if (cursor.moveToFirst()) {
+                                do {
+                                    mime = cursor.getString(mimeIdx);
+                                    if(returnKeys.containsKey(mime)) {
+                                        contactData.putString((String) returnKeys.get(mime), cursor.getString(dataIdx));
+                                        foundData = true;
+                                    }
+                                } while (cursor.moveToNext());
+                            }
+                            
+                            if(foundData) {
+                                mContactsPromise.resolve(contactData);
+                                return;
+                            } else {
+                                mContactsPromise.reject(E_CONTACT_NO_DATA, "No data found for contact");
+                                return;
+                            }
+                        } catch (Exception e) {
+                            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
+                            return;
+                        }
+                        /* No need to break as all paths return */
+                    case(EMAIL_REQUEST):
+                        /* Return contacts first email address, as string */
+                        try {
+                            
+                            
+                            // get the contact id from the Uri
+                            String id = contactUri.getLastPathSegment();
+                            
+                            // query for everything email
+                            Cursor cursor = mCtx.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                                                                            null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[]{id},
+                                                                            null);
+                            
+                            int emailIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
+                            
+                            // For now, return only the first email address, as a string
+                            if (cursor.moveToFirst()) {
+                                email = cursor.getString(emailIdx);
+                                mContactsPromise.resolve(email);
+                                return;
+                            } else {
+                                //Contact has no email address stored
+                                mContactsPromise.reject(E_CONTACT_NO_EMAIL, "No email found for contact");
+                                return;
+                            }
+                        } catch (Exception e) {
+                            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
+                            return;
+                        }
+                        /* No need to break as all paths return */
+                    default:
+                        //Unexpected return code - shouldn't happen, but catch just in case
+                        mContactsPromise.reject(E_CONTACT_EXCEPTION, "Unexpected error in request");
                         return;
-                    } else {
-                        mContactsPromise.reject(E_CONTACT_NO_EMAIL, "No Email Address Found For User");
-                        return;
-                    }*/
-
-                } catch (Exception e) {
-                    mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
-                    return;
                 }
             default:
+                //Request was cancelled
                 mContactsPromise.reject(E_CONTACT_CANCELLED, "Cancelled");
                 return;
         }
--- ios/RCTContactsWrapper/RCTContactsWrapper.m
+++ ios/RCTContactsWrapper/RCTContactsWrapper.m
@@ -6,45 +6,82 @@
 //  Copyright © 2016 Facebook. All rights reserved.
 //
 
+@import Foundation;
 #import "RCTContactsWrapper.h"
 @interface RCTContactsWrapper()
 
 @property(nonatomic, retain) RCTPromiseResolveBlock _resolve;
 @property(nonatomic, retain) RCTPromiseRejectBlock _reject;
+
 @end
 
 
 @implementation RCTContactsWrapper
 
+int _requestCode;
+const int REQUEST_CONTACT = 1;
+const int REQUEST_EMAIL = 2;
 
 
 RCT_EXPORT_MODULE(ContactsWrapper);
 
-
-RCT_EXPORT_METHOD(getEmail:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+/* Get basic contact data as JS object */
+RCT_EXPORT_METHOD(getContact:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
   {
     self._resolve = resolve;
     self._reject = reject;
-    UIViewController *picker;
-    if([CNContactPickerViewController class]) {
-      picker = [[CNContactPickerViewController alloc] init];
-      ((CNContactPickerViewController *)picker).delegate = self;
-    } else {
-      picker = [[ABPeoplePickerNavigationController alloc] init];
-      [((ABPeoplePickerNavigationController *)picker) setPeoplePickerDelegate:self];
-    }
-    UIViewController *root = [[[UIApplication sharedApplication] delegate] window].rootViewController;
-    [root presentViewController:picker animated:YES completion:nil];
+    _requestCode = REQUEST_CONTACT;
     
+    [self launchContacts];
     
     
   }
 
+/* Get ontact email as string */
+RCT_EXPORT_METHOD(getEmail:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+  self._resolve = resolve;
+  self._reject = reject;
+  _requestCode = REQUEST_EMAIL;
+  
+  [self launchContacts];
+  
+  
+}
 
-#pragma mark - Event handlers
+
+/**
+ Launch the contacts UI
+ */
+-(void) launchContacts {
+  
+  UIViewController *picker;
+  if([CNContactPickerViewController class]) {
+    //iOS 9+
+    picker = [[CNContactPickerViewController alloc] init];
+    ((CNContactPickerViewController *)picker).delegate = self;
+  } else {
+    //iOS 8 and below
+    picker = [[ABPeoplePickerNavigationController alloc] init];
+    [((ABPeoplePickerNavigationController *)picker) setPeoplePickerDelegate:self];
+  }
+  //Launch Contact Picker or Address Book View Controller
+  UIViewController *root = [[[UIApplication sharedApplication] delegate] window].rootViewController;
+  [root presentViewController:picker animated:YES completion:nil];
+  
+  
+}
+
+
+#pragma mark - RN Promise Events
 
 - (void)pickerCancelled {
   self._reject(@"E_CONTACT_CANCELLED", @"Cancelled", nil);
+}
+
+
+- (void)pickerError {
+  self._reject(@"E_CONTACT_EXCEPTION", @"Unknown Error", nil);
 }
 
 - (void)pickerNoEmail {
@@ -56,14 +93,79 @@ RCT_EXPORT_METHOD(getEmail:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseR
 }
 
 
+-(void)contactPicked:(NSDictionary *)contactData {
+  self._resolve(contactData);
+}
+
+
+#pragma mark - Shared functions
+
+
+- (NSMutableDictionary *) emptyContactDict {
+  return [[NSMutableDictionary alloc] initWithObjects:@[@"", @"", @""] forKeys:@[@"name", @"phone", @"email"]];
+}
+
+/**
+ Return full name as single string from first last and middle name strings, which may be empty
+ */
+-(NSString *) getFullNameForFirst:(NSString *)fName middle:(NSString *)mName last:(NSString *)lName {
+  //Check whether to include middle name or not
+  NSArray *names = (mName.length > 0) ? [NSArray arrayWithObjects:fName, mName, lName, nil] : [NSArray arrayWithObjects:fName, lName, nil];;
+  return [names componentsJoinedByString:@" "];
+}
+
+
+
+#pragma mark - Event handlers - iOS 9+
 - (void)contactPicker:(CNContactPickerViewController *)picker didSelectContact:(CNContact *)contact {
-  if([contact.emailAddresses count] < 1) {
-    [self pickerNoEmail];
-    return;
+  switch(_requestCode){
+    case REQUEST_CONTACT:
+    {
+      /* Return NSDictionary ans JS Object to RN, containing basic contact data
+       This is a starting point, in future more fields should be added, as required.
+       This could also be extended to return arrays of phone numbers, email addresses etc. instead of jsut first found
+       */
+      NSMutableDictionary *contactData = [self emptyContactDict];
+      
+      NSString *fullName = [self getFullNameForFirst:contact.givenName middle:contact.middleName last:contact.familyName ];
+      NSArray *phoneNos = contact.phoneNumbers;
+      NSArray *emailAddresses = contact.emailAddresses;
+      
+      //Return full name
+      [contactData setValue:fullName forKey:@"name"];
+      
+      //Return first phone number
+      if([phoneNos count] > 0) {
+        CNPhoneNumber *phone = ((CNLabeledValue *)phoneNos[0]).value;
+        [contactData setValue:phone.stringValue forKey:@"phone"];
+      }
+      
+      //Return first email address
+      if([emailAddresses count] > 0) {
+        [contactData setValue:((CNLabeledValue *)emailAddresses[0]).value forKey:@"email"];
+      }
+      
+      [self contactPicked:contactData];
+    }
+      break;
+    case REQUEST_EMAIL :
+    {
+      /* Return Only email address as string */
+      if([contact.emailAddresses count] < 1) {
+        [self pickerNoEmail];
+        return;
+      }
+      
+      CNLabeledValue *email = contact.emailAddresses[0].value;
+      [self emailPicked:email];
+    }
+      break;
+    default:
+      //Should never happen, but just in case, reject promise
+      [self pickerError];
+    break;
   }
   
-  CNLabeledValue *email = contact.emailAddresses[0].value;
-  [self emailPicked:email];
   
 }
 
@@ -74,15 +176,66 @@ RCT_EXPORT_METHOD(getEmail:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseR
 
 
 
+#pragma mark - Event handlers - iOS 8
+
+/* Same functionality as above, implemented using iOS8 AddressBook library */
 - (void)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker didSelectPerson:(ABRecordRef)person {
-  ABMultiValueRef emailMultiValue = ABRecordCopyValue(person, kABPersonEmailProperty);
-  NSArray *emailAddresses = (__bridge NSArray *)ABMultiValueCopyArrayOfAllValues(emailMultiValue);
-  if([emailAddresses count] < 1) {
-    [self pickerNoEmail];
-     return;
+  switch(_requestCode) {
+    case(REQUEST_CONTACT):
+    {
+      
+      /* Return NSDictionary ans JS Object to RN, containing basic contact data
+       This is a starting point, in future more fields should be added, as required.
+       This could also be extended to return arrays of phone numbers, email addresses etc. instead of jsut first found
+       */
+      NSMutableDictionary *contactData = [self emptyContactDict];
+            NSString *fNameObject, *mNameObject, *lNameObject;
+      fNameObject = (__bridge NSString *) ABRecordCopyValue(person, kABPersonFirstNameProperty);
+      mNameObject = (__bridge NSString *) ABRecordCopyValue(person, kABPersonMiddleNameProperty);
+      lNameObject = (__bridge NSString *) ABRecordCopyValue(person, kABPersonLastNameProperty);
+      
+      NSString *fullName = [self getFullNameForFirst:fNameObject middle:mNameObject last:lNameObject];
+      
+      //Return full name
+      [contactData setValue:fullName forKey:@"name"];
+      
+      //Return first phone number
+      ABMultiValueRef phoneMultiValue = ABRecordCopyValue(person, kABPersonPhoneProperty);
+      NSArray *phoneNos = (__bridge NSArray *)ABMultiValueCopyArrayOfAllValues(phoneMultiValue);
+      if([phoneNos count] > 0) {
+        [contactData setValue:phoneNos[0] forKey:@"phone"];
+      }
+     
+      //Return first email
+      ABMultiValueRef emailMultiValue = ABRecordCopyValue(person, kABPersonEmailProperty);
+      NSArray *emailAddresses = (__bridge NSArray *)ABMultiValueCopyArrayOfAllValues(emailMultiValue);
+      if([emailAddresses count] > 0) {
+        [contactData setValue:emailAddresses[0] forKey:@"email"];
+      }
+      
+
+      [self contactPicked:contactData];
+    }
+      break;
+    case(REQUEST_EMAIL):
+    {
+      /* Return Only email address as string */
+      ABMultiValueRef emailMultiValue = ABRecordCopyValue(person, kABPersonEmailProperty);
+      NSArray *emailAddresses = (__bridge NSArray *)ABMultiValueCopyArrayOfAllValues(emailMultiValue);
+      if([emailAddresses count] < 1) {
+        [self pickerNoEmail];
+        return;
+      }
+      
+      [self emailPicked:emailAddresses[0]];
+    }
+      break;
+
+    default:
+      //Should never happen, but just in case, reject promise
+      [self pickerError];
+      return;
   }
-  
-  [self emailPicked:emailAddresses[0]];
   
 }
 
