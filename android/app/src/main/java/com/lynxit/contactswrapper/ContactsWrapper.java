605d94fd9ba3cb2f5c74c52c5fa27fc1b7c8fb75
Bryce Pavey <brycep@lynxit.com.au>
2016-11-16 15:45:34 +1100

    Fix compilation error on RN versions >0.31

--- android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
+++ android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
@@ -29,7 +29,7 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.ViewManager;
 
 public class ContactsWrapper extends ReactContextBaseJavaModule implements ActivityEventListener {
-    
+
     private static final int CONTACT_REQUEST = 1;
     private static final int EMAIL_REQUEST = 2;
     private static final String E_CONTACT_CANCELLED = "E_CONTACT_CANCELLED";
@@ -38,30 +38,30 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
     private static final String E_CONTACT_EXCEPTION = "E_CONTACT_EXCEPTION";
     private Promise mContactsPromise;
     private Activity mCtx;
-    
-    
+
+
     public ContactsWrapper(ReactApplicationContext reactContext) {
         super(reactContext);
         reactContext.addActivityEventListener(this);
     }
-    
+
     @Override
     public String getName() {
         return "ContactsWrapper";
     }
-    
-    
-    
+
+
+
     @ReactMethod
     public void getContact(Promise contactsPromise) {
         launchPicker(contactsPromise, CONTACT_REQUEST);
     }
-    
+
     @ReactMethod
     public void getEmail(Promise contactsPromise) {
         launchPicker(contactsPromise, EMAIL_REQUEST);
     }
-    
+
     /**
      * Lanch the contact picker, with the specified requestCode for returned data.
      * @param contactsPromise - promise passed in from React Native.
@@ -76,15 +76,15 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
             mCtx.startActivityForResult(intent, requestCode);
         }
     }
-    
+
     @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
+    public void onActivityResult(Activity ContactsWrapper, final int requestCode, final int resultCode, final Intent intent) {
 
         if(mContactsPromise == null || mCtx == null
               || (requestCode != CONTACT_REQUEST && requestCode != EMAIL_REQUEST)){
           return;
         }
-        
+
         String email = null;
         switch (resultCode) {
             case (Activity.RESULT_OK):
@@ -93,13 +93,13 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                     case(CONTACT_REQUEST):
                         try {
                             /* Retrieve all possible data about contact and return as a JS object */
-                            
+
                             /* Map Any contact data we want returned to the JS object key for React Native */
                             HashMap<String, String> returnKeys = new HashMap<String, String>();
                             returnKeys.put(ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, "name");
                             returnKeys.put(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, "phone");
                             returnKeys.put(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, "email");
-                            
+
                             //First get ID
                             String id = null;
                             int idx;
@@ -112,12 +112,12 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                                 mContactsPromise.reject(E_CONTACT_NO_DATA, "Contact Data Not Found");
                                 return;
                             }
-                            
+
                             // Build the Entity URI.
                             Uri.Builder b = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, id).buildUpon();
                             b.appendPath(ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
                             contactUri = b.build();
-                            
+
                             // Create the projection (SQL fields) and sort order.
                             String[] projection = {
                                 ContactsContract.Contacts.Entity.MIMETYPE,
@@ -125,7 +125,7 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                             };
                             String sortOrder = ContactsContract.Contacts.Entity.RAW_CONTACT_ID + " ASC";
                             cursor = mCtx.getContentResolver().query(contactUri, projection, null, null, sortOrder);
-                            
+
                             String mime;
                             boolean foundData = false;
                             int dataIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.DATA1);
@@ -139,7 +139,7 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                                     }
                                 } while (cursor.moveToNext());
                             }
-                            
+
                             if(foundData) {
                                 mContactsPromise.resolve(contactData);
                                 return;
@@ -155,18 +155,18 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                     case(EMAIL_REQUEST):
                         /* Return contacts first email address, as string */
                         try {
-                            
-                            
+
+
                             // get the contact id from the Uri
                             String id = contactUri.getLastPathSegment();
-                            
+
                             // query for everything email
                             Cursor cursor = mCtx.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
                                                                             null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[]{id},
                                                                             null);
-                            
+
                             int emailIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
-                            
+
                             // For now, return only the first email address, as a string
                             if (cursor.moveToFirst()) {
                                 email = cursor.getString(emailIdx);
